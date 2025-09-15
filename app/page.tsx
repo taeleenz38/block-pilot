f588789a13cf2dc3bea53d542c13abc100eb52e7
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-09-15 21:54:06 +1000

    homepage / chat window WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import ChatWindow from "@/components/layout/ChatWindow";
+
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-primary">
+    <div className="h-screen w-screen bg-primary pl-44 flex flex-col gap-2 justify-center items-center">
+      <h1 className="text-5xl font-semibold">Welcome to Block Pilot</h1>
+      <ChatWindow />
     </div>
   );
 }
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -6,10 +6,10 @@ const Navbar = () => {
   return (
     <div className="fixed left-0 top-0 p-10 w-44 h-screen bg-primary border-r-2 border-primary-border z-50 flex flex-col justify-between">
       <div>
-        <Link href="/" className="w-full flex justify-center mb-10">
-          <Image src="/logo.png" alt="logo" width={50} height={50} />
+        <Link href="/" className="w-full flex justify-center mb-12">
+          <Image src="/logo.png" alt="logo" width={60} height={60} />
         </Link>
-        <div className="flex flex-col gap-6 font-semibold text-lg">
+        <div className="flex flex-col gap-6 font-semibold text-xl">
           <Link href="/dashboard" className="hover:text-blue-300 duration-150">
             Dashboard
           </Link>
